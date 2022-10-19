2e684e080a12d385a3b15809c30c5c90652afdb8
Dani Bagley <bagley.a.danielle@gmail.com>
2022-10-19 17:52:38 -0600

    Create variables to store fetched data

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,29 +1,44 @@
 // Your fetch requests will live here!
 import RecipeRepository from "../src/classes/RecipeRepository";
+import User from "./classes/User";
+import Ingredient from "../src/classes/Ingredient-class";
+import Recipe from "./classes/Recipe";
 const allRecipes = new RecipeRepository();
-console.log(allRecipes);
+const ingredientsList = [];
+const allRecipesList = [];
+const currentUsers = [];
 
 fetch("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/users")
   .then((res) => res.json())
   .then((data) => {
-    console.log(data);
+    // currentUser.generateRandomUser(data.usersData);
+    setUpUser(data.usersData);
   });
+
+function setUpUser(dataSet) {
+  const currentUser = new User();
+  currentUser.generateRandomUser(dataSet);
+  currentUsers.push(currentUser);
+}
 
 console.log("I will be a fetch request!");
 
 fetch("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/ingredients")
   .then((res) => res.json())
   .then((data) => {
-    console.log(data);
+    setUp(data.ingredientsData, ingredientsList, Ingredient);
   });
+
+function setUp(dataSet, listArray, classType) {
+  dataSet.forEach((element) => {
+    listArray.push(new classType(element));
+  });
+}
 
 fetch("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/recipes")
   .then((res) => res.json())
   .then((data) => {
-    console.log(data);
-    allRecipes.addRecipes(data.recipeData);
+    setUp(data.recipeData, allRecipesList, Recipe);
   });
 
-console.log("WHAAAAT", allRecipes);
-
-export { allRecipes };
+export { allRecipes, ingredientsList, allRecipesList, currentUsers };
--- src/scripts.js
+++ src/scripts.js
@@ -1,16 +1,17 @@
 import "./styles.css";
 import apiCalls from "./apiCalls";
-import { allRecipes } from "./apiCalls";
+import {
+  allRecipes,
+  ingredientsList,
+  allRecipesList,
+  currentUsers,
+} from "./apiCalls";
 import RecipeRepository from "./classes/RecipeRepository";
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import "./images/turing-logo.png";
 
 console.log("Hello world");
 
-// const allRecipes = new RecipeRepository();
-// console.log(allRecipes);
-
-console.log(
-  "YAAAAAAAAA",
-  allRecipes.filterByName("Loaded Chocolate Chip Pudding Cookie Cups")
-);
+console.log("YAAHSGKAJSHGAK", ingredientsList);
+console.log("JHJJJJJJ", allRecipesList);
+console.log("dskljhafkajshdf", currentUsers);
