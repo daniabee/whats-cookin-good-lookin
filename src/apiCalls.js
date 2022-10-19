a56979f710728bd4c7b9dd89688f98a5aec6fbf6
Dani Bagley <bagley.a.danielle@gmail.com>
2022-10-19 16:44:51 -0600

    Add fetch calls to the the api

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,21 +1,29 @@
 // Your fetch requests will live here!
+import RecipeRepository from "../src/classes/RecipeRepository";
+const allRecipes = new RecipeRepository();
+console.log(allRecipes);
+
 fetch("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/users")
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-    })
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+  });
 
-
-console.log('I will be a fetch request!')
+console.log("I will be a fetch request!");
 
 fetch("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/ingredients")
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-    })
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+  });
 
 fetch("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/recipes")
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-    })
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    allRecipes.addRecipes(data.recipeData);
+  });
+
+console.log("WHAAAAT", allRecipes);
+
+export { allRecipes };
--- src/scripts.js
+++ src/scripts.js
@@ -1,10 +1,16 @@
 import "./styles.css";
 import apiCalls from "./apiCalls";
+import { allRecipes } from "./apiCalls";
 import RecipeRepository from "./classes/RecipeRepository";
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import "./images/turing-logo.png";
 
 console.log("Hello world");
 
-const allRecipes = new RecipeRepository();
-console.log(allRecipes);
+// const allRecipes = new RecipeRepository();
+// console.log(allRecipes);
+
+console.log(
+  "YAAAAAAAAA",
+  allRecipes.filterByName("Loaded Chocolate Chip Pudding Cookie Cups")
+);
