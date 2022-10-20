d54655fd1bfe81b2a99515bb5d83d1552c1ebe32
Dani Bagley <bagley.a.danielle@gmail.com>
2022-10-19 20:35:24 -0600

    Add a promise all and refactored the apiCalls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,34 +3,44 @@ import RecipeRepository from "../src/classes/RecipeRepository";
 import User from "./classes/User";
 import Ingredient from "../src/classes/Ingredient-class";
 import Recipe from "./classes/Recipe";
-const ingredientsList = [];
-const allRecipesList = [];
-const users = [];
+// const ingredientsList = [];
+// const allRecipesList = [];
+// const users = [];
 
-fetch("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/users")
-  .then((res) => res.json())
-  .then((data) => {
-    setUp(data.usersData, users, User);
-  });
+var loadData = (url) => {
+  return fetch(url)
+    .then((response) => response.json())
+    .catch((error) =>
+      console.log("There was an error loading the data!", error)
+    );
+};
 
-console.log("I will be a fetch request!");
+// fetch("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/users")
+//   .then((res) => res.json())
+//   .then((data) => {
+//     setUp(data.usersData, users, User);
+//   });
 
-fetch("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/ingredients")
-  .then((res) => res.json())
-  .then((data) => {
-    setUp(data.ingredientsData, ingredientsList, Ingredient);
-  });
+// console.log("I will be a fetch request!");
 
-function setUp(dataSet, listArray, classType) {
-  dataSet.forEach((element) => {
-    listArray.push(new classType(element));
-  });
-}
+// fetch("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/ingredients")
+//   .then((res) => res.json())
+//   .then((data) => {
+//     setUp(data.ingredientsData, ingredientsList, Ingredient);
+//   });
 
-fetch("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/recipes")
-  .then((res) => res.json())
-  .then((data) => {
-    setUp(data.recipeData, allRecipesList, Recipe);
-  });
+// function setUp(dataSet, listArray, classType) {
+//   dataSet.forEach((element) => {
+//     listArray.push(new classType(element));
+//   });
+// }
 
-export { ingredientsList, allRecipesList, users };
+// fetch("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/recipes")
+//   .then((res) => res.json())
+//   .then((data) => {
+//     setUp(data.recipeData, allRecipesList, Recipe);
+//   });
+
+// export { ingredientsList, allRecipesList, users };
+
+export default loadData;
--- src/scripts.js
+++ src/scripts.js
@@ -1,15 +1,35 @@
 import "./styles.css";
-import apiCalls, { users } from "./apiCalls";
-import { ingredientsList, allRecipesList, user } from "./apiCalls";
-import RecipeRepository from "./classes/RecipeRepository";
+// import apiCalls, { users } from "./apiCalls";
+// import { ingredientsList, allRecipesList, user } from "./apiCalls";
+// import RecipeRepository from "./classes/RecipeRepository";
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
+import loadData from "./apiCalls";
 import "./images/turing-logo.png";
 import User from "./classes/User";
 
 console.log("Hello world");
 
-console.log("INGREDIENTS", ingredientsList);
-console.log("RECIPES", allRecipesList);
-console.log("USERS", users);
+// console.log("INGREDIENTS", ingredientsList);
+// console.log("RECIPES", allRecipesList);
+// console.log("USERS", users);
 
-const allRecipes = new RecipeRepository(allRecipesList);
+let userData;
+let ingredientsData;
+let recipeData;
+
+// const allRecipes = new RecipeRepository(allRecipesList);
+Promise.all([
+  loadData("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/users"),
+  loadData(
+    "https://what-s-cookin-starter-kit.herokuapp.com/api/v1/ingredients"
+  ),
+  loadData("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/recipes"),
+]).then((data) => {
+  userData = data[0];
+  ingredientsData = data[1];
+  recipeData = data[2];
+
+  console.log(userData);
+  console.log(ingredientsData);
+  console.log(recipeData);
+});
