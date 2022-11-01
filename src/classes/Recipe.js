3f03e8cd504ee8f24b2d587a5e87238fb7d36447
Dani Bagley <bagley.a.danielle@gmail.com>
2022-11-01 16:41:47 -0600

    add refactored test

--- src/classes/Recipe.js
+++ src/classes/Recipe.js
@@ -32,17 +32,3 @@ class Recipe {
 }
 
 export default Recipe;
-
-// There are some devs that believe that
-//tests should be broken down more.
-//For example, the test 'Should be able to filter recipes by
-//their name' tests for successful finds and a non-successful find.
-//This could be broken down into 'Should be able to filter recipes by their name' and a
-//second test such as 'Should not find a recipe by name if it doesn't match' or something
-//like that. Discuss as a group and decide if this style fits your coding opinions.
-
-//Continue to deepen your testing. For example, right now the test “should have an image”
-//simply tests if the image is not null. This means I could have a
-//string that just says “cheese!” and it would pass.
-//Is that enough? Consider adding to that test to check if a
-//specific image is what you expect.
